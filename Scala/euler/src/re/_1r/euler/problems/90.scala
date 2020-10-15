cef2db8342a4b6e0a5170956f6ac1fbc16a64c51
tjm1518 <tjm1518@ic.ac.uk>
2020-10-15 17:11:00 +0100

    90

--- Scala/euler/src/re/_1r/euler/problems/90.scala
+++ Scala/euler/src/re/_1r/euler/problems/90.scala
@@ -2,31 +2,42 @@ package re._1r.euler
 package problems
 
 object Euler90 {
-  val squares = (1 to 9).map(x => (100 + x * x).toString.toCharArray.tail)
-  val dn = collection.mutable.ArrayBuffer[Array[Array[Char]]]()
-  def arrangements(c0: Array[Char], c1: Array[Char]) = {
-    squares.forall(chr => {
-      (c0.contains(chr(0)) || chr(0) == '9' && c0.contains('6')) &&
-      (c1.contains(chr(1)) || chr(1) == '9' && c1.contains('6')) ||
-      (c0.contains(chr(1)) || chr(1) == '9' && c0.contains('6')) &&
-      (c1.contains(chr(0)) || chr(0) == '9' && c1.contains('6'))
-    })
+  def squares = Array(1, 4, 9, 16, 25, 36, 49, 64, 81)
+  def find(c0: Array[Int], c1: Array[Int]): Boolean = {
+    (c0.contains(0) && c1.contains(1) || c0.contains(1) && c1.contains(0)) &&                                          //1
+    (c0.contains(0) && c1.contains(4) || c0.contains(4) && c1.contains(0)) &&                                          //4
+    (c0.contains(0) && (c1.contains(6) || c1.contains(9)) || (c0.contains(6) || c0.contains(9)) && c1.contains(0)) &&  //9
+    (c0.contains(1) && (c1.contains(6) || c1.contains(9)) || (c0.contains(6) || c0.contains(9)) && c1.contains(1))    //16
+    (c0.contains(5) && c1.contains(2) || c0.contains(2) && c1.contains(5)) &&                                         //25
+    (c0.contains(3) && (c1.contains(6) || c1.contains(9)) || (c0.contains(6) || c0.contains(9)) && c1.contains(3)) && //36
+    (c0.contains(4) && (c1.contains(6) || c1.contains(9)) || (c0.contains(6) || c0.contains(9)) && c1.contains(4)) && //49
+    (c0.contains(4) && (c1.contains(6) || c1.contains(9)) || (c0.contains(6) || c0.contains(9)) && c1.contains(4)) && //64
+    (c0.contains(8) && c1.contains(1) || c0.contains(1) && c1.contains(8))                                            //81
   }
+
   def result = {
-    var rtn = 0
-    val c00 = 0
-    for(c01 <- 1 until 9; c02 <- c01 + 1 until 9; c03 <- c02 + 1 until 9; c04 <- c03 + 1 until 9; c05 <- c04 + 1 until 9) {
-      val c0 = Array(c00.toString.apply(0), c01.toString.apply(0), c02.toString.apply(0), c03.toString.apply(0), c04.toString.apply(0), c05.toString.apply(0))
-      for(c10 <- 0 until 9; c11 <- c10 + 1 until 9; c12 <- c11 + 1 until 9; c13 <- c12 + 1 until 9; c14 <- c13 + 1 until 9; c15 <- c14 + 1 until 9) {
-        val c1 = Array(c10.toString.apply(0), c11.toString.apply(0), c12.toString.apply(0), c13.toString.apply(0), c14.toString.apply(0), c15.toString.apply(0))
-        if (arrangements(c1, c0) && dn.forall(x => {
-          !(x.contains(c1) && x.contains(c0))
-        })) {
-          rtn += 1
-          dn += Array(c0, c1)
-        }
+    var rtn = collection.mutable.ArrayBuffer[Array[Array[Int]]]()
+    for(
+      c00 <- 0 to 9;
+      c01 <- c00 + 1 to 9;
+      c02 <- c01 + 1 to 9;
+      c03 <- c02 + 1 to 9;
+      c04 <- c03 + 1 to 9;
+      c05 <- c04 + 1 to 9
+    ) {
+      def c0 = Array(c00, c01, c02, c03, c04, c05)
+      for(
+        c00 <- 0 to 9;
+        c01 <- c00 + 1 to 9;
+        c02 <- c01 + 1 to 9;
+        c03 <- c02 + 1 to 9;
+        c04 <- c03 + 1 to 9;
+        c05 <- c04 + 1 to 9
+      ) {
+        def c1 = Array(c00, c01, c02, c03, c04, c05)
+        if(find(c0, c1) && c0 != c1) rtn += Array(c0, c1).sortBy(_.sum)
       }
     }
-    rtn
+    rtn.distinct.size
   }
 }
