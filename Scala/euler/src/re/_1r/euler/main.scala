57b030d8702c287eeaf7d09ae4054b00882b75c8
tjm1518 <tjm1518@ic.ac.uk>
2020-10-15 16:43:04 +0100

    .

--- Scala/euler/src/re/_1r/euler/main.scala
+++ Scala/euler/src/re/_1r/euler/main.scala
@@ -2,17 +2,9 @@ package re._1r.euler
 import problems._
 import scala.compat.Platform.currentTime
 
-<<<<<<< HEAD
 object Main extends App{
 	val now = currentTime
-	val result = _68.result
+	val result = Euler90.result
 	val nownow = currentTime
 	println(s"$result\ntook: ${nownow - now}")
-=======
-object main extends App {
-	val start = java.lang.System.currentTimeMillis
-	val result = Euler88.result
-	val fin = java.lang.System.currentTimeMillis
-	println(result + "\ntook: " + (fin - start).toDouble / 1000)
->>>>>>> 586e1ba890d950595e7b542f79e1b28444ce88f1
 }
--- Scala/euler/src/re/_1r/euler/problems/88.scala
+++ Scala/euler/src/re/_1r/euler/problems/88.scala
@@ -1,7 +1,6 @@
 package re._1r.euler
 package problems
 
-<<<<<<< HEAD
 import math.BigInt
 
 object Euler88 {
@@ -29,50 +28,5 @@ object Euler88 {
       println(s"${x}: ${y}")
       y
     })).distinct.sum
-=======
-import collection.mutable.ArrayBuffer
-import math.BigInt
-
-object Euler88 {
-  def compareSums(v: ArrayBuffer[BigInt], end: Int, n: Int): BigInt = {
-    val product = v.product
-    val sum = v.sum
-    println(product.toString + " & " + sum.toString + "\n" +  v.mkString(","))
-    if (end == v.length - 1 && sum > product) {
-      v(end) = n
-      compareSums(v, 0, n)
-    } else if (end == v.length - 1 && sum < product && v(end) > 1) {
-      v(end) -= 1
-      compareSums(v, 0, 1)
-    } else if (end == v.length - 1 && sum < product && n > 1) {
-      v(end) = 2
-      compareSums(v, 0, 1)
-    } else if (end == v.length - 1 && sum < product) {
-      v(end) = n
-      compareSums(v, 0, 1)
-    } else if(sum > product) {
-      v(end) = n
-      compareSums(v, end + 1, n + 1)
-    } else if (sum < product && n > 1) {
-      v(end) = n - 1
-      compareSums(v, end + 1, 2)
-    } else if (sum < product) {
-      v(end) = n
-      compareSums(v, end + 1, 1)
-    } else sum
-  }
-  def nn = 200
-  def start = 2
-  def result = {
-    val vec = ArrayBuffer.fill(start - 1)(BigInt(1))
-    (start to nn).map(BigInt(_)).fold(BigInt(0))((acc, i) => {
-      vec.mapInPlace(_ => 1)
-      //println(vec.mkString(","))
-      vec += (1)
-      val x = compareSums(vec, 0, 2)
-      println(s"${i}: ${x}")
-      acc + x
-    })
->>>>>>> 586e1ba890d950595e7b542f79e1b28444ce88f1
   }
 }
